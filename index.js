57d5269368683aff32d45a85dcd5d202dfbfde90
Meng Zhou <v-menzhou@expediagroup.com>
2021-12-22 14:07:58 -0600

    commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello,world!');
+const _ = require('lodash');
+
+// Original array
+let array1 = [
+  [1, 2],
+  [4, 5],
+  [7, 8],
+];
+
+let newArray = _.flatten(array1);
+
+console.log(newArray);
